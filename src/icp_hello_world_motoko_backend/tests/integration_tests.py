0f2c4474480b8656dbe3ff22c652bc154f07549b
Elias Datler <elias.datler@dfinity.org>
2024-01-25 13:38:39 +0000

    make script executable

--- src/icp_hello_world_motoko_backend/tests/integration_tests.py
+++ src/icp_hello_world_motoko_backend/tests/integration_tests.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from pocket_ic import PocketIC
 import unittest
 from os import environ, path
@@ -8,7 +10,7 @@ BASE_PATH = "/workspaces/icp-hello-world-motoko/.dfx/local/canisters/icp_hello_w
 class BackendCanisterTests(unittest.TestCase):
     def setUp(self) -> None:
         if not path.isdir(BASE_PATH):
-            raise Exception("Please run dfx deploy before running tests")
+            raise Exception('Please run "dfx deploy" before running tests')
         with open(BASE_PATH + "icp_hello_world_motoko_backend.did", "r") as candid:
             self.candid = candid.read()
         with open(BASE_PATH + "icp_hello_world_motoko_backend.wasm", "rb") as wasm:
